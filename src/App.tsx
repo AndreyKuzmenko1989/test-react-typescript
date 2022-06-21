5f5d42a2f532d78592cf82176017c48f3555d749
AndreyKuzmenko1989 <kuzmenkophp@gmail.com>
2022-06-21 17:34:53 +0300

    add login page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import {ROUTES} from "./routes/constants";
 import {Layout} from "./common/layout";
+import Login from "./pages/login"
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Router>
           <Routes>
               <Route path={ROUTES.main} element={<Layout/>}>
+                  <Login></Login>
               </Route>
           </Routes>
       </Router>
---
+++ src/pages/login.tsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import {Login} from '../features/auth/components/Login';
+
+const LoginPage = () => {
+    return (
+        <Fragment>
+            <title>Login</title>
+            <Login />
+        </Fragment>
+    );
+};
+
+export default LoginPage;
